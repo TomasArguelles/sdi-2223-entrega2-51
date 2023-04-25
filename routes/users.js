fdc0d33021c3efd0f422a1efe7f46a4d55e7fb27
UO253628 <uo253628@uniovi.es>
2023-04-25 10:45:05 +0200

    Añadidos campos extra en el registro

--- routes/users.js
+++ routes/users.js
@@ -13,26 +13,41 @@ module.exports = function (app, usersRepository) {
     })
 
     app.post('/users/signup', function (req, res) {
-        let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
-            .update(req.body.password).digest('hex');
-        let user = {
-            email: req.body.email,
-            password: securePassword
-        }
-        usersRepository.findUser({email: user.email}, {}).then(us => {
-            if (us === null)
-                usersRepository.insertUser(user).then(userId => {
-                    res.redirect("/users/login" +
-                        "?message=Nuevo usuario registrado"+
-                        "&messageType=alert-info ");
-                })
-            else
-                res.redirect("/users/login");
-        }).catch(error => {
-            res.redirect("/users/singup" +
+        if(req.body.password !== req.body.password2)
+            res.redirect("/users/signup" +
                 "?message=Se ha producido un error al registrar el usuario"+
                 "&messageType=alert-danger ");
-        });
+        else {
+            let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+                .update(req.body.password).digest('hex');
+            let user = {
+                email: req.body.email,
+                name: req.body.name,
+                surname: req.body.surname,
+                date: req.body.date,
+                money: 100,
+                kind: "Usuario Estándar",
+                password: securePassword
+            }
+            //email, nombre, apellidos, fecha de
+            // nacimiento (DD/MM/AAAA), y una contraseña
+            usersRepository.findUser({email: user.email}, {}).then(us => {
+                if (us === null)
+                    usersRepository.insertUser(user).then(userId => {
+                        res.redirect("/users/login" +
+                            "?message=Nuevo usuario registrado" +
+                            "&messageType=alert-info ");
+                    })
+                else
+                    res.redirect("/users/signup" +
+                        "?message=Se ha producido un error al registrar el usuario" +
+                        "&messageType=alert-danger ");
+            }).catch(error => {
+                res.redirect("/users/signup" +
+                    "?message=Se ha producido un error al registrar el usuario" +
+                    "&messageType=alert-danger ");
+            });
+        }
     });
 
     app.get('/users/login', function (req, res) {
